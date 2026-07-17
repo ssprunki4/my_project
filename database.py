3b0abe74681b1470394ae99821410cbbc806b2d1
Ihor <ssprunki111@gmail.com>
2026-07-17 20:09:17 +0200

    Fix: DATABASE_URL will changed in database.py

--- database.py
+++ database.py
@@ -1,8 +1,19 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine, AsyncSession
 from sqlalchemy.orm import declarative_base
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
-DATABASE_URL = 'postgresql+asyncpg://postgres:admin@localhost:5432/guides_db'
-engine = create_async_engine(DATABASE_URL)
+
+
+class Setting(BaseSettings):
+    database_url: str
+    model_config = SettingsConfigDict(env_file='.env')
+
+
+settings = Setting()
+engine = create_async_engine(settings.database_url)
+
+
 AsyncSessionLocal = async_sessionmaker(bind=engine, expire_on_commit=False, class_=AsyncSession)
 
 Base = declarative_base()
